66b9ea001ae5869805ca80157b8dffd18003ffa5
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2025-09-06 11:14:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/cafe-cafeteria-and-restaurant-furniture',
+  basePath: '/restaurant-furniture',
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
